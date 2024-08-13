268e78ec0af6e8e3f84287ef14abdde907cd276a
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-08-12 21:35:27 -0600

    toDoList
    fixed function that wasn't clearing list items

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -18,7 +18,7 @@ function addTask () {
 listContainer.addEventListener('click', function(e) {
     if(e.target.tagName === "LI") {
         e.target.classList.toggle('checked');
-    } else if(e.target.tagName === "span") {
+    } else if(e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
     }
 }, false);
