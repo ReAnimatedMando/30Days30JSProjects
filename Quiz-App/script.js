90999ae1f9e8d0af2ea67f0a5a294fde60640f6f
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-08-14 21:14:54 -0600

    quizApp
    added questions 2 & 3

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -7,5 +7,26 @@ const questions = [
             {text: "Elephant", correct: false},
             {text: "Giraffe", correct: false}
         ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false}
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antartica", correct: true}
+        ]
+    },
+    {
+        
     }
 ]
